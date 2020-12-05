914a5edf804b9a413b896214302cce1df44417fd
huangzhewei0127 <137753475@qq.com>
2020-12-06 00:49:36 +0800

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_10_080336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "code", limit: 100
+    t.integer "product_id"
+    t.integer "number"
+    t.decimal "amount", precision: 14, scale: 4
+    t.integer "user_id"
+    t.string "aasm_state", limit: 100
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "contact", limit: 100
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "title", limit: 50
+    t.integer "sorting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", limit: 100
+    t.integer "number"
+    t.string "description"
+    t.boolean "on_shelf"
+    t.decimal "price", precision: 14, scale: 4
+    t.integer "product_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "location", limit: 500
+    t.string "img", limit: 500
+    t.index ["product_category_id"], name: "index_products_on_product_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "avatar"
+    t.string "phone"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_admin"
+  end
 
 end
